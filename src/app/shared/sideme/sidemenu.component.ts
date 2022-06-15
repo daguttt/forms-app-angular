7299492e48275b5a449ba49afd080252dd3d489a
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-06-15 12:05:20 -0500

    Class 231: First Modules and Components

---
+++ src/app/shared/sideme/sidemenu.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+})
+export class SidemenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
