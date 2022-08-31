e4bb8ee867ac46cc66debbeddfb6691f75cd7de0
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-08-31 15:47:47 -0500

    Class 255: Submitting form

--- src/app/reactive/basics/reactive-basics.component.ts
+++ src/app/reactive/basics/reactive-basics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './reactive-basics.component.html',
   styles: [],
 })
-export class ReactiveBasicsComponent {
+export class ReactiveBasicsComponent implements OnInit {
   // myForm: FormGroup = new FormGroup({
   //   product: new FormControl('RTX 3090ti'),
   //   price: new FormControl(1500),
@@ -19,8 +19,30 @@ export class ReactiveBasicsComponent {
   });
   constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    // ! With .setValue() we need to pass ALL properties
+    // this.myForm.setValue({
+    //   product: 'RTX 3090ti',
+    //   price: 3000,
+    //   stock: 2
+    // }) -> !
+    this.myForm.reset({
+      product: 'RTX 3090ti',
+      price: 3000,
+    });
+  }
+
   isInputValid(inputName: string) {
     const input = this.myForm.controls[inputName];
     return input.errors && input.touched;
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
   }
 }
