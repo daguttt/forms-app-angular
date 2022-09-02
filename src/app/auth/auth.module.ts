ab037a715d6f44dd208b1fbd6d3ccd518cb442bd
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-02 12:02:09 -0500

    Class 269: Validate with a regular expression

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginPageComponent,
-    SignupPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginPageComponent, SignupPageComponent],
+  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/pages/signup-page/signup-page.component.ts
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
 })
 export class SignupPageComponent implements OnInit {
-  constructor() {}
+  fullnamePattern: RegExp =
+    /([^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+) ([^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+)/;
+
+  signupForm: FormGroup = this.fb.group({
+    fullname: [
+      '',
+      [Validators.required, Validators.pattern(this.fullnamePattern)],
+    ],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  isInputValid(inputName: string): boolean | undefined {
+    return (
+      this.signupForm.get(inputName)?.invalid &&
+      this.signupForm.get(inputName)?.touched
+    );
+  }
+
+  onSubmit(): void {
+    console.log(this.signupForm.value);
+    this.signupForm.markAllAsTouched();
+  }
 }
