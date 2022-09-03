c982386d059f73efda028d5c08f6f1181967fb26
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-03 09:01:11 -0500

    Class 270: Validate email

--- src/app/auth/pages/signup-page/signup-page.component.ts
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -9,16 +9,28 @@ export class SignupPageComponent implements OnInit {
   fullnamePattern: RegExp =
     /([^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+) ([^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+)/;
 
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
   signupForm: FormGroup = this.fb.group({
     fullname: [
       '',
       [Validators.required, Validators.pattern(this.fullnamePattern)],
     ],
+    email: [
+      '',
+      // * [Validators.required, Validators.email, -> Kind of worse
+      [Validators.required, Validators.pattern(this.emailPattern)],
+    ],
   });
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.signupForm.reset({
+      fullname: 'Daniel Gutiérrez',
+      email: 'dagutmu667@gmail.com',
+    });
+  }
 
   isInputValid(inputName: string): boolean | undefined {
     return (
